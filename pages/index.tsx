c02a6e4c9e9fa040deaa26363e1fa0ca580815d5
wangyh8456 <282943394@qq.com>
2023-02-21 17:43:42 +0800

    change index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home:React.FC<IndexProps>=({allPostsData})=>{
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>a cool man</p>
+        <p>hello world</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
